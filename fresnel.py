3919c1828b84bed684b584cd43ae3fa2cc3dfa97
agustrionopj <agustriono.pj@gmail.com>
2017-05-11 16:23:04 +0200

    1st trial of LC modeling

---
+++ fresnel.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 30 17:02:33 2014
+Edited on Thu May 11, 2017
+
+@author: PJ
+"""
+
+from pylab import *
+from scipy.special import fresnel
+import matplotlib.pyplot as plt
+
+## baca file masukan
+#-------------------
+import numpy as np
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+
+pjgGel, weight1, weight2 = loadtxt('transcurveb.txt', unpack=True)
+
+x = np.linspace(-100., 100., 50000)
+
+pjgGel_weight = pjgGel * weight1
+
+pjgGel_tes = 15000.
+
+n = len(x)
+m = len(pjgGel_weight)
+
+result1 = zeros((n, m))      # nrows, ncols --> kebalikan dari IDL
+result2 = zeros((n, m))
+
+for i in range (m):
+    for j in range (n):
+        w1 = x[j] * 1e10 * sqrt(2. / (pjgGel_weight[i] * 3.84e18))
+        w2 = x[j] * 1e10 * sqrt(2. / (pjgGel_tes * 3.84e18))
+        s1, c1 = fresnel(w1)
+        s2, c2 = fresnel(w2)
+        result1[j, i] = 0.5 * (0.5 + s1 + 0.5 + c1) ** 2
+        result2[j, i] = 0.5 * (0.5 + s2 + 0.5 + c2) ** 2
+total_res1 = result1.sum(axis = 1)
+total_res2 = result2.sum(axis = 1)
+
+plt.plot(x, total_res1)
+plt.plot(x, total_res2)
+plt.grid(True)
+plt.show()
+
+# x_data, y_data = loadtxt('data_LO.txt', unpack=True) # buka file masukan
+# plt.plot(x_data, y_data, 'ro') #plot file masukan
+# plt.show()
+
